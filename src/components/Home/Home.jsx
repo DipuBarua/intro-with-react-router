f5f12b2f712667657096b76bca627e64ea8fb07f
DipuBarua <you@example.com>
2023-09-21 14:25:41 +0600

    used navigation,location hooks to home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
 
 const Home = () => {
+    // navigation hook for router loading
+    const navigation = useNavigation();
+    // location hook for getting pathName
+    const location = useLocation();
+    console.log(location);
+
     return (
         <div>
             <Header></Header>
+            {
+                navigation.state === 'loading' ? <p>Loading....</p> : navigation.state === 'submitting' ? <p>submitting...</p> : ''
+            }
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
